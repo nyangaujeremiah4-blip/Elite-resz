d0c0f96058f6d8febebbe288b86acf836b807380
nyangaujeremiah4-blip <nyangaujeremiah4@gmail.com>
2026-02-17 16:09:56 +0300

    Update supabase_mobile.js

--- supabase_mobile.js
+++ supabase_mobile.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-const supabaseUrl = "YOUR_SUPABASE_URL";     // replace with your actual Supabase project URL
-const supabaseKey = "YOUR_SUPABASE_ANON_KEY"; // replace with your actual anon key
+const supabaseUrl = "https://sptnezzamlgpxeftktgv.supabase.co";     // replace with your actual Supabase project URL
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNwdG5lenphbWxncHhlZnRrdGd2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA4OTM4NDMsImV4cCI6MjA4NjQ2OTg0M30.ycf-czE7weYBFs4B0DbPAhe9R7KrVS4qqcasgAGDG88"; // replace with 
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
